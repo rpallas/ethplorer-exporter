b812fd079e0234066a4ce5cbf48f2d40b4a49162
Robbie Pallas <robbie.pallas@piksel.com>
2018-01-26 02:27:09 +0000

    refactor

--- ethplorer.js
+++ ethplorer.js
@@ -1,37 +1,38 @@
-'use strict';
-
 const server = require('express')();
 const { register, Gauge, collectDefaultMetrics } = require('prom-client');
 
 const got = require('got');
-let response;
 
-(async () => {
+async function collect() {
   try {
-    response = await got('https://api.ethplorer.io/getAddressInfo/0xCa5b4dDA6CF6Ef6b76F289a3aD228b31B327e05d?apiKey=freekey', { json: true });
-    ethMetrics(response);
+    const response = await got('https://api.ethplorer.io/getAddressInfo/0xCa5b4dDA6CF6Ef6b76F289a3aD228b31B327e05d?apiKey=freekey', { json: true });
+    return response;
   } catch (error) {
     console.log(`Error: ${error}`);
   }
-})();
+}
+
+const prefix = 'ethplorer_account_';
+const ethBalanceGauge = new Gauge({ name: `${prefix}eth_balance`, help: 'Total balance of either in account', labelNames: ['address'] });
+const ethTotalInGauge = new Gauge({ name: `${prefix}eth_totalIn`, help: 'Total either transferred into account', labelNames: ['address'] });
+const ethTotalOutGauge = new Gauge({ name: `${prefix}eth_totalOut`, help: 'Total either transferred out of account', labelNames: ['address'] });
 
 function ethMetrics(response) {
-  const { address, ETH, countTxs, tokens } = response.body;
-  const prefix = 'ethplorer_account_';
-  
-  const balanceGauge = new Gauge({ name: `${prefix}eth_balance`, help: 'Total balance of either in account', labelNames: ['address'] });
-  balanceGauge.set({ address: address }, ETH.balance);
-  const totalInGauge = new Gauge({ name: `${prefix}eth_totalIn`, help: 'Total either transferred into account', labelNames: ['address'] });
-  totalInGauge.set({ address: address }, ETH.totalIn);
-  const totalOutGauge = new Gauge({ name: `${prefix}eth_totalOut`, help: 'Total either transferred out of account', labelNames: ['address'] });
-  totalOutGauge.set({ address: address }, ETH.totalOut);
-  
-  console.log(`balanceGauge: ${JSON.stringify(balanceGauge.get())},  totalInGauge: ${JSON.stringify(totalInGauge.get())},  totalOutGauge: ${JSON.stringify(totalOutGauge.get())},`);
+  const { address, ETH } = response.body;
+
+  ethBalanceGauge.set({ address: address }, ETH.balance);
+  ethTotalInGauge.set({ address: address }, ETH.totalIn);
+  ethTotalOutGauge.set({ address: address }, ETH.totalOut);
+
+  console.log(JSON.stringify(ethTotalOutGauge.get()));
 }
 
 server.get('/metrics', (req, res) => {
-  res.set('Content-Type', register.contentType);
-  res.end(register.metrics());
+  collect().then((response) => {
+    ethMetrics(response);
+    res.set('Content-Type', register.contentType);
+    res.end(register.metrics());
+  });
 });
 
 // Enable collection of default metrics
